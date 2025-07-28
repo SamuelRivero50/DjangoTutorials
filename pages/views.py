e2da8ebcf370f2cc57d78ea79f1045e0867dc645
RiveroDev <samuelenriquerivero@gmail.com>
2025-07-28 16:36:01 -0500

    Agregar about page y rutas

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,16 @@ def homePageView(request):
     return HttpResponse('Hello World!') 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        return context
